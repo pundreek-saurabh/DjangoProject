f4b039e25db7f8ad8c91536c1959ac593f4c30e8
pundreek-saurabh <pundreeksaurabh@gmail.com>
2020-12-31 18:44:21 +0530

    last commit of 2020 :)

--- polls/models.py
+++ polls/models.py
@@ -5,13 +5,11 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    # ...
     question_text = models.CharField(max_length=200,default=False)
 
     def was_published_recently(self):
         return self.question_text
 
 class Choice(models.Model):
-    # ...
     def __str__(self):
         return self.choice_text
